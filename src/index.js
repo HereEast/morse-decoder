73b6760555f841dc18f5c29b45efb7ab36d9967a
Here East <nastiapiven@gmail.com>
2022-02-23 15:53:54 +0100

    feat: cleaneed code

--- src/index.js
+++ src/index.js
@@ -42,23 +42,18 @@ function decode(expr) {
     let lettersArr = [];
 
     for(let i = 0; i < expr.length; i += 10) {
-        let letter = expr.substr(i, 10);
+        let letter = expr.slice(i, i + 10);
         lettersArr.push(letter);
     }
-    //console.log(lettersArr);
 
     function removeZero(str) {
         let strArr = str.split('');
         let idx = strArr.findIndex(el => el === '1');
-        //console.log(idx);
         strArr.splice(0, idx);
         return strArr.join('');
     }
 
-    //console.log(removeZero('00000011005050411'));
-
     let noZeros = lettersArr.map(el => removeZero(el));
-    //console.log(noZeros);
 
     let ten = /10/gi;
     let eleven = /11/gi;
@@ -66,16 +61,11 @@ function decode(expr) {
     let dash = '-';
 
     let replacedWithDots = noZeros.map(el => el.replace(ten, dot));
-    let replacedDotsDashes = replacedWithDots.map(el => el.replace(eleven, dash));
+    let dotsDashes = replacedWithDots.map(el => el.replace(eleven, dash));
+    let mapLetters = dotsDashes.map(el => MORSE_TABLE[el]);
 
-    //console.log(replacedDotsDashes);
-
-    let mapLetters = replacedDotsDashes.map(el => MORSE_TABLE[el]);
-    let joined = mapLetters.join('');
-    console.log(joined);
-    
-    return joined;
-}4
+    return mapLetters.join('');
+}
 
 module.exports = {
     decode
